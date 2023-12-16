f63a8ce4ccb4dc1b4e3759f5659c3a2e05dfd01e
Liu Wenyuan <15816141883@163.com>
2023-12-16 23:25:39 +0800

    Use init for type def

--- win32/init.lua
+++ win32/init.lua
@@ -1,7 +1,53 @@
+local ffi = require("ffi")
+
 -- General Win32 interactions
 --
 -- Most stuff in the root scope comes from kernel32
-OSExt.Win32 = {}
+local win32 = {}
+OSExt.Win32 = win32
+
+win32.Libs = {}
+
+---@alias OSExt.Win32.DWORD ffi.cdata*
+ffi.cdef[[
+    typedef unsigned int UINT;
+    typedef unsigned long DWORD;
+    typedef unsigned long *PULONG;
+]]
+
+---@alias OSExt.Win32.BOOL ffi.cdata*
+ffi.cdef[[
+    // 0=false 1=true
+    typedef bool BOOL;
+    typedef BOOL BOOLEAN;
+
+    typedef BOOL *PBOOL;
+    typedef BOOL *LPBOOL;
+]]
+
+ffi.cdef[[
+    typedef wchar_t WCHAR;
+    typedef WCHAR *LPWSTR;
+
+    typedef char CHAR;
+    typedef CHAR *LPSTR;
+
+    typedef const char *LPCCH;
+    typedef const wchar_t *LPCWCH;
+]]
+
+ffi.cdef[[
+    typedef void *HANDLE;
+
+    typedef const void *LPCVOID;
+]]
+
+win32.HResults = {
+    ERROR_SUCCESS = 0,
+    ERROR_INVALID_PARAMETER = 0x57,
+    ERROR_MORE_DATA = 0xea,
+    ERROR_MR_MID_NOT_FOUND = 0x13d
+}
 
 libRequire("osext", "win32/kernel32")
 libRequire("osext", "win32/psapi")
--- win32/kernel32.lua
+++ win32/kernel32.lua
@@ -1,47 +1,23 @@
 local ffi = require("ffi")
 local bit = require("bit")
 
+-- kernel32 is pretty much for OS stuff
+
 local win32 = OSExt.Win32
 
-win32.Libs = {}
 win32.Libs.kernel32 = ffi.load("kernel32")
 if not win32.Libs.kernel32 then
     error("kernel32 not available?!")
 end
 
-win32.HResults = {
-    ERROR_SUCCESS = 0,
-    ERROR_INVALID_PARAMETER = 0x57,
-    ERROR_MORE_DATA = 0xea,
-    ERROR_MR_MID_NOT_FOUND = 0x13d
-}
-
----@alias OSExt.Win32.DWORD ffi.cdata*
-ffi.cdef[[
-    typedef unsigned int UINT;
-    typedef unsigned long DWORD;
-    typedef unsigned long *PULONG;
-]]
-
----@alias OSExt.Win32.BOOL ffi.cdata*
-ffi.cdef[[
-    // 0=false 1=true
-    typedef bool BOOL;
-    typedef BOOL BOOLEAN;
-    typedef BOOL *PBOOL;
-    typedef BOOL *LPBOOL;
-]]
-
----@alias OSExt.Win32.HANDLE ffi.cdata*
-ffi.cdef[[
-    typedef void *HANDLE;
-
-    BOOL CloseHandle(HANDLE hObject);
-]]
 win32.HANDLE = ffi.typeof("HANDLE")
 -- A handle that is invalid
 ---@type OSExt.Win32.HANDLE
 win32.INVALID_HANDLE_VALUE = ffi.cast(win32.HANDLE, -1)
+---@alias OSExt.Win32.HANDLE ffi.cdata*
+ffi.cdef[[
+    BOOL CloseHandle(HANDLE hObject);
+]]
 -- Makes a handle cdata that is automatically GC'd
 ---@param handle ffi.cdata*
 ---@return OSExt.Win32.HANDLE
@@ -52,13 +28,6 @@ function win32.makeHandle(handle)
 end
 
 ffi.cdef[[
-    typedef wchar_t WCHAR;
-    typedef WCHAR *LPWSTR;
-    typedef char CHAR;
-    typedef CHAR *LPSTR;
-    typedef const char *LPCCH;
-    typedef const wchar_t *LPCWCH;
-    
     int WideCharToMultiByte(
         UINT CodePage,
         DWORD dwFlags,
@@ -106,8 +75,6 @@ function win32.luaToWideString(str)
 end
 
 ffi.cdef[[
-    typedef const void *LPCVOID;
-
     DWORD FormatMessageW(
         DWORD dwFlags,
         LPCVOID lpSource,
