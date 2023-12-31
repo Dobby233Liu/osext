c44245ec556aba0f0e0e623aa883e058a850421e
Liu Wenyuan <15816141883@163.com>
2023-12-31 23:37:09 +0800

    WIP OSExt.Win32.Status

---
+++ win32/dataclasses/status.lua
@@ -0,0 +1,43 @@
+-- WIP
+--
+-- [MS-ERREF](https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/1bc92ddf-b79e-413c-bbaa-99a5281a6c90)
+OSExt.Win32.Status = Class()
+
+---@enum OSExt.Win32.Status.SEVERITY
+-- Maps to NTSTATUS' STATUS_SEVERITY_*
+OSExt.Win32.Status.SEVERITY = {
+    -- means success in HRESULT
+    success = 0,
+    informational = 1,
+    -- means failure in HRESULT
+    warning = 2,
+    error = 3
+}
+---@enum (key) OSExt.Win32.Status.FACILITY_MODES
+OSExt.Win32.Status.FACILITY_KINDS = {
+    hResult = 0,
+    ntStatus = 1
+}
+
+---@private
+function OSExt.Win32.Status:init()
+    self.severity = OSExt.Win32.Status.SEVERITY.success
+    self.customer = false
+    self.facility = 0 -- FIXME
+    self.facility_kind = OSExt.Win32.Status.FACILITY_KINDS.hResult
+    self.code = 0 -- FIXME
+end
+
+function OSExt.Win32.Status:fromWin32(w32Error)
+    assert(w32Error >= 0)
+    local status = OSExt.Win32.Status()
+    if w32Error == 0 --[[ FIXME ]] then
+        status.severity = OSExt.Win32.Status.SEVERITY.success
+    else
+        status.severity = OSExt.Win32.Status.SEVERITY.failure
+    end
+    status.facility_kind = OSExt.Win32.Status.FACILITY_KINDS.hResult
+    status.facility = OSExt.Win32.NtStatusFacilities.FACILITY_WIN32 -- FIXME
+    status.code = w32Error
+    return status
+end
