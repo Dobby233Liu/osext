2154df2b6d47bfc3095fa8a3155f9996791fb51b
Liu Wenyuan <15816141883@163.com>
2024-05-25 15:43:01 +0800

    ok

--- unix/host.lua
+++ unix/host.lua
@@ -2,7 +2,7 @@ local ffi = require "ffi"
 
 -- with null terminator
 
-OSExt.Unix.MAX_HOSTNAME = 64 + 1
+OSExt.Unix.MAX_HOSTNAME = 64
 OSExt.Unix.MAX_DOMAINNAME = 64
 
 ffi.cdef[[
@@ -13,22 +13,28 @@ ffi.cdef[[
 -- Gets the hostname of the machine.
 ---@return string hostName
 function OSExt.Unix.getHostName()
-    print(1)
+    if ffi.os == "Linux" then
+        -- on Linux glibc implements this by looking in uname
+        return OSExt.Unix.LinuxOSVer.getKernelVersion().nodeName
+    end
+
+    -- FIXME
     local buf = ffi.new("char[?]", OSExt.Unix.MAX_HOSTNAME)
-    ffi.fill(buf, OSExt.Unix.MAX_HOSTNAME, 0)
-    print(1)
     local ok = ffi.C.gethostname(name, OSExt.Unix.MAX_HOSTNAME)
-    print(1)
     if ok ~= 0 then OSExt.Unix.raiseLastError() end
-    print(1)
     return ffi.string(buf)
 end
 
 -- Gets the domain name of the machine.
 ---@return string domainName
 function OSExt.Unix.getDomainName()
+    if ffi.os == "Linux" then
+        -- glibc implements this by looking in uname if the domainname field exists in utsname
+        return OSExt.Unix.LinuxOSVer.getKernelVersion().domainName
+    end
+
+    -- FIXME
     local buf = ffi.new("char[?]", OSExt.Unix.MAX_DOMAINNAME)
-    ffi.fill(buf, OSExt.Unix.MAX_DOMAINNAME, 0)
     local ok = ffi.C.getdomainname(name, OSExt.Unix.MAX_DOMAINNAME)
     if ok ~= 0 then OSExt.Unix.raiseLastError() end
     return ffi.string(buf)
