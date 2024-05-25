87721c4f1faa3a25d4f2e233911b2130427d551b
Liu Wenyuan <15816141883@163.com>
2024-05-25 14:57:49 +0800

    ok

--- unix/osver.lua
+++ unix/osver.lua
@@ -9,11 +9,11 @@ ffi.cdef[[
 ]]
 
 -- FIXME: don't fucking do this
--- "Part of the utsname information is also accessible via /proc/sys/kernel/{ostype, hostname, osrelease, version, domainname}.""
+-- "Part of the utsname information is also accessible via /proc/sys/kernel/{ostype, hostname, osrelease, version, domainname}."
 function OSExt.Unix.getKernelVersion()
     -- this one is a gamble
     -- 5 entries x 65, not certain about the GNU extension
-    local uname_raw = ffi.new("utsname_raw[?]", 65 * 5)
+    local uname_raw = ffi.new("char[?]", 65 * 5)
     local ok = ffi.C.uname(uname_raw)
     if ok ~= 0 then OSExt.Unix.raiseLastError() end
     local keys = {"sysname", "nodename", "release", "version", "machine" --[[, "domainname" ]]}
