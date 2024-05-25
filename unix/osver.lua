a7f691f0146e480e48734c27ae4053a4b196c010
Liu Wenyuan <15816141883@163.com>
2024-05-25 15:03:14 +0800

    ok

--- unix/osver.lua
+++ unix/osver.lua
@@ -3,26 +3,3 @@ local ffi = require "ffi"
 if ffi.os == "Linux" then
     libRequire("osext", "unix/osver_linux")
 end
-
-ffi.cdef[[
-    int uname(char *buf);
-]]
-
--- FIXME: don't fucking do this
--- "Part of the utsname information is also accessible via /proc/sys/kernel/{ostype, hostname, osrelease, version, domainname}."
-function OSExt.Unix.getKernelVersion()
-    -- this one is a gamble
-    -- 5 entries x 65, not certain about the GNU extension
-    local uname_raw = ffi.new("char[?]", 65 * 5)
-    local ok = ffi.C.uname(uname_raw)
-    if ok ~= 0 then OSExt.Unix.raiseLastError() end
-    local keys = {"sysname", "nodename", "release", "version", "machine" --[[, "domainname" ]]}
-    local ret = {}
-    local pos = 0
-    for _,key in ipairs(keys) do
-        local str_here = ffi.string(uname_raw + pos)
-        ret[key] = str_here
-        pos = pos + #str_here + 1
-    end
-    return ret
-end
--- unix/osver_linux.lua
+++ unix/osver_linux.lua
@@ -5,6 +5,39 @@ local fs = OSExt.Unix.fs
 
 OSExt.Unix.LinuxOSVer = {}
 
+-- TODO: "Part of the utsname information is also accessible via /proc/sys/kernel/{ostype, hostname, osrelease,
+--              version, domainname}."
+if not OSExt._typeExists("struct utsname") then
+    ffi.cdef[[
+        struct utsname {
+            char sysname[65];
+            char nodename[65];
+            char release[65];
+            char version[65];
+            char machine[65];
+            char domainname[65];
+        };
+    ]]
+end
+ffi.cdef[[
+    int uname(struct utsname *buf);
+]]
+
+-- Returns the name and information about the current kernel.
+function OSExt.Unix.LinuxOSVer.getKernelVersion()
+    local struc = ffi.new("struct utsname")
+    local ret = ffi.C.uname(struc)
+    if ret ~= 0 then OSExt.Unix.raiseLastError() end
+    return {
+        sysname = ffi.string(struc.sysname),
+        nodename = ffi.string(struc.nodename),
+        release = ffi.string(struc.release),
+        version = ffi.string(struc.version),
+        machine = ffi.string(struc.machine),
+        domainname = ffi.string(struc.domainname)
+    }
+end
+
 local function parseBshKV(str)
     local ret = {}
     for _,line in ipairs(Utils.split(str, "\n")) do
